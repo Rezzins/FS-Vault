a5cbcb7e3c44ace99e2106df4f3aea2f802e98b1
Aidan <aidanjames7303@gmail.com>
2025-07-23 11:40:23 -0500

    Vault Code Secured

---
+++ index.js
@@ -0,0 +1,25 @@
+//code 1 
+let code1;
+
+code1 = 7 + 3;
+
+//code 2
+const code2 = (100 - 20) / 2;
+
+//code 3
+const code3 = 39 % 47;
+
+//Vault Message
+const message = " The vault has been secured. The combination is: " ;
+
+//Code A Defined
+const codeA =  code1 + "-" + code2 + "-" + code3 ;
+
+//Code B Defined (Differntly)
+const codeB = `${code1}-${code2}-${code3}`;
+
+//Console Logs
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
